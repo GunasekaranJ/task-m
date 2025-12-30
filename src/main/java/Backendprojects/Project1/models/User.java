0c739e66a0420fa10c340aae051d31ba6b0dd320
GunasekaranJ <gunasekarangs219@gmail.com>
2025-12-30 12:17:46 +0530

    Build with Java 17

--- src/main/java/Backendprojects/Project1/models/User.java
+++ src/main/java/Backendprojects/Project1/models/User.java
@@ -6,11 +6,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "Usertable")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue
     private Long user_id;
@@ -21,69 +28,4 @@ public class User {
 
     @NotNull
     private String password;
-
-    // No-args constructor
-    public User() {}
-
-    // All-args constructor
-    public User(Long user_id, String email, String password) {
-        this.user_id = user_id;
-        this.email = email;
-        this.password = password;
-    }
-
-    // Getters and Setters
-    public Long getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    // Builder pattern manually
-    public static UserBuilder builder() {
-        return new UserBuilder();
-    }
-
-    public static class UserBuilder {
-        private Long user_id;
-        private String email;
-        private String password;
-
-        public UserBuilder user_id(Long user_id) {
-            this.user_id = user_id;
-            return this;
-        }
-
-        public UserBuilder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserBuilder password(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public User build() {
-            return new User(user_id, email, password);
-        }
-    }
 }
